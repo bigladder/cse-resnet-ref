76810e0d350df924efe2f557e31ccb7a75dc7f57
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2026-01-26 12:19:45 -0700

    Pass in minimum rated temperature, duct type, and motor type into RESNETDXModel.

--- functions/get_air_source_heat_pump_performance_map.py
+++ functions/get_air_source_heat_pump_performance_map.py
@@ -1,7 +1,13 @@
 from sys import argv
 
 from koozie import fr_u
-from resdx import RESNETDXModel, StagingType, get_heating_performance_map_object, get_cooling_performance_map_object
+from resdx import (
+    FanMotorType,
+    RESNETDXModel,
+    StagingType,
+    get_heating_performance_map_object,
+    get_cooling_performance_map_object,
+)
 
 
 def get_performance_map(
@@ -9,9 +15,12 @@ def get_performance_map(
     cooling_capacity_95_btuh: str,
     heating_capacity_47_btuh: str,
     heating_capacity_17_btuh: str,
+    minimum_rated_temperature_degF: str,
     seer2: str,
     eer2: str,
     hspf2: str,
+    motor_type: str,
+    duct_type: str,
 ) -> str:
     """
     Get CSE objects rsPerfMapClg and rsPerfMapHtg
@@ -22,18 +31,30 @@ def get_performance_map(
     heating_capacity_47 = fr_u(float(heating_capacity_47_btuh), "kBtu/h")
     heating_capacity_17 = fr_u(float(heating_capacity_17_btuh), "kBtu/h")
 
+    minimum_rated_temperature = float(minimum_rated_temperature_degF)
+
     seer2: float = float(seer2)  # type: ignore
     eer2: float = float(eer2)  # type: ignore
     hspf2: float = float(hspf2)  # type: ignore
+
+    motor_type = FanMotorType[motor_type]
+
+    if duct_type == "DUCTED":
+        is_ducted = True
+    else:  # duct_type == "DUCTLESS"
+        is_ducted = False
 
     unit = RESNETDXModel(
         staging_type=stage_type,
         rated_net_total_cooling_capacity=cooling_capacity_95,
         rated_net_heating_capacity=heating_capacity_47,
         rated_net_heating_capacity_17=heating_capacity_17,
+        heating_off_temperature=minimum_rated_temperature,
         input_seer=seer2,
         input_eer=eer2,
         input_hspf=hspf2,
+        motor_type=motor_type,
+        is_ducted=is_ducted,
     )
 
     system_name = "RSYS"
@@ -51,9 +72,12 @@ if __name__ == "__main__":
     cooling_capacity_95_btuh = argv[2]
     heating_capacity_47_btuh = argv[3]
     heating_capacity_17_btuh = argv[4]
-    seer2 = argv[5]
-    eer2 = argv[6]
-    hspf2 = argv[7]
+    minimum_rated_temperature_degF = argv[5]
+    seer2 = argv[6]
+    eer2 = argv[7]
+    hspf2 = argv[8]
+    motor_type = argv[9]
+    duct_type = argv[10]
 
     print(
         get_performance_map(
@@ -61,8 +85,11 @@ if __name__ == "__main__":
             cooling_capacity_95_btuh=cooling_capacity_95_btuh,
             heating_capacity_47_btuh=heating_capacity_47_btuh,
             heating_capacity_17_btuh=heating_capacity_17_btuh,
+            minimum_rated_temperature_degF=minimum_rated_temperature_degF,
             seer2=seer2,
             eer2=eer2,
             hspf2=hspf2,
+            motor_type=motor_type,
+            duct_type=duct_type,
         )
     )
